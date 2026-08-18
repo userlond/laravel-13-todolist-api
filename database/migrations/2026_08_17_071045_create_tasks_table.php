66dcfcae69c940e28186244593a945eccc8ac46a
Ilya G. <userlond@yandex.ru>
2026-08-18 11:27:36 +0700

    Fix task description type

--- database/migrations/2026_08_17_071045_create_tasks_table.php
+++ database/migrations/2026_08_17_071045_create_tasks_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->timestamps();
         });
     }
